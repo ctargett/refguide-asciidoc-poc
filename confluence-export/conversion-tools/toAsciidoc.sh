6896d6f6195f463d955252076973c1d29eca9375
Chris Hostetter <hossman_github@fucit.org>
2016-08-19 11:50:05 -0700

    Elminate 'work.dir' param from build.xml and script now that i better understand where Cassandra was keeping/running these

--- confluence-export/conversion-tools/toAsciidoc.sh
+++ confluence-export/conversion-tools/toAsciidoc.sh
@@ -5,14 +5,9 @@
 # process html to asciidoc
 # reconvert asciidoc to html
 
-set -e
-if [ -z $1 ]
-then
-  echo "Must specify a work dir on the command line"
-  exit -1
-fi
 
-WORK_DIR=$1
+# parent dir of script until/unless we move it
+WORK_DIR=$(realpath -L "$(dirname $0)/../")
 
 if [ ! -d $WORK_DIR ]
 then
@@ -28,41 +23,25 @@ then
     exit -1
 fi
 
-PANDOC_TEMPLATE="$(dirname $0)/custom.pandoc.template"
+PANDOC_TEMPLATE="$WORK_DIR/conversion-tools/custom.pandoc.template"
 if [ ! -e $PANDOC_TEMPLATE ]
 then
     echo "$PANDOC_TEMPLATE does not exist"
     exit -1
 fi
-   
-# function to use multiple times
-convert_dir() {
-    if [ -z "$1" ]
-    then
-	echo "convert_dir called w/o html dir"
-	exit -1
-    fi
-    if [ -z "$2" ]
-    then
-	echo "convert_dir called w/o ascii dir"
-	exit -1
-    fi
-    HTML_DIR=$1
-    ASCII_DIR=$2
 
-    rm -rf $ASCII_DIR
+HTML_DIR="$WORK_DIR/cleaned-export"
+ASCII_DIR="$WORK_DIR/converted-asciidoc"
+
+rm -rf $ASCII_DIR
+
+for x in `find $HTML_DIR -name "*.html"`
+do
+    echo $x;
+    FNAME=`echo ${x} | sed -e "s#${HTML_DIR}/##"`
+    DIRNAME=$(dirname ${FNAME})
+    mkdir -p "$ASCII_DIR/$DIRNAME"
     
-    for x in `find $HTML_DIR -name "*.html"`
-    do
-	echo $x;
-	FNAME=`echo ${x} | sed -e "s#${HTML_DIR}/##"`
-	DIRNAME=$(dirname ${FNAME})
-	mkdir -p "$ASCII_DIR/$DIRNAME"
-	
-	# convert to .asciidoc format using pandoc
-	pandoc $HTML_DIR/$FNAME -f html -t asciidoc -i --parse-raw --wrap=none --standalone --atx-headers --template=$PANDOC_TEMPLATE -o ${ASCII_DIR}/${FNAME%.*}.adoc
-    done;
-}
-
-
-convert_dir "$WORK_DIR/cleaned-flat-export" "$WORK_DIR/cleaned-flat-asciidoc"
+    # convert to .asciidoc format using pandoc
+    pandoc $HTML_DIR/$FNAME -f html -t asciidoc -i --parse-raw --wrap=none --standalone --atx-headers --template=$PANDOC_TEMPLATE -o ${ASCII_DIR}/${FNAME%.*}.adoc
+done;
