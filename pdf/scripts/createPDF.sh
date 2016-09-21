14a58d7fad0a97533dc30d71df5afd412d188a8c
Cassandra Targett <cassandra.targett@lucidworks.com>
2016-09-21 14:46:15 -0500

    Remove unused PDF dirs

--- pdf/scripts/createPDF.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf pdf/*.pdf
-rm -rf pdf/*.pdfmarks
-
-asciidoctor-pdf -a source-highlighter=coderay -a icons=font -a imagesDir=asciidoc/images -a pdf-stylesDir=pdf/themes -a pdf-style=refguide -a pdf-fontsDir=pdf/fonts -a pagenums -a figure-caption! -D ./pdf -o SolrRefGuide.pdf SolrRefGuide.adoc
